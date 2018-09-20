4ea219abcd446a092499165b014325feb5fce963
Rose <rose1264@gmail.com>
2018-09-20 17:48:59 -0400

    setup reducer and store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
+// touch src/components/SearchBar.js
+// touch src/components/Listings.js
+// touch src/components/Listing.js
+// touch src/components/ListingDetail.js
+// touch src/components/ListingInfo.js
+// touch src/components/ListingMap.js
+// touch src/components/CreateReservationForm.js
+//
+// touch src/components/CreateListingForm.js
+// touch src/components/MyReservations.js
+// touch src/components/MyReviews.js
+//
+// touch src/components/MyTrips.js
+// touch src/components/PostReviewForm.js
+
+
+//thinking in React
+//App <==state : {
+  // guest_id: user_id
+  // host_id: user_id
+  // listings: []
+  // reservations:[]
+  // trips:[]
+  // current_listing: {}
+  // current_reservation: {}
+  // current_trip:{}
+  // reviews:[]
+//}
+//  Form <== local state
