d33689e64008b6d85cf2c7322981362bbe554555
Rose <rose1264@gmail.com>
2018-10-01 11:35:23 -0400

    error handling for listing detail page

--- src/components/ListingDetail.js
+++ src/components/ListingDetail.js
@@ -5,15 +5,28 @@ import ListingInfo from './ListingInfo'
 // TODO:import ListnigMap from './ListingMap'
 import CreateReservationForm from './CreateReservationForm'
 import { Container } from 'semantic-ui-react'
+import { Redirect } from 'react-router-dom'
+import Listings from './Listings'
 
 
 const ListingDetail = props => {
-  return(
-    <Container>
-      <ListingInfo />
-      <CreateReservationForm />
-    </Container>
-  )
+    if (!props.listing.host){
+      return <Redirect to='/listings' component={Listings}/>
+    } else {
+      return(
+        <Container>
+          <ListingInfo />
+          <CreateReservationForm />
+        </Container>
+      )
+    }
+
 }
 
-export default withAuth(connect()(ListingDetail))
+function mapStateToProps(state) {
+  return {
+    listing: state.listingsReducer.currentListing
+  }
+}
+
+export default withAuth(connect(mapStateToProps)(ListingDetail))
