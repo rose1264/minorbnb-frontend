cc66dea5cc6293800b5ad71e5ee94dc5ec3ff00a
Rose <rose1264@gmail.com>
2018-10-02 19:22:04 -0400

    add mapping & marker

--- src/components/ListingMap.js
+++ src/components/ListingMap.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import GoogleMapReact from 'google-map-react';
+import { Icon } from 'semantic-ui-react'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Marker = props => {
+  return <Icon name='map marker alternate'/>
+}
 
 class ListingMap extends Component {
   static defaultProps = {
@@ -10,30 +13,31 @@ class ListingMap extends Component {
         lat: 40.7128,
         lng: -74.0060
       },
-      zoom: 12
+      zoom: 14
     };
 
     render() {
       let lat = this.props.listing.lat
       let lng = this.props.listing.lng
+      let center = {lat, lng}
       return (
-        <div style={{ height: '50vh', width: '50%' }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: `${process.env.REACT_APP_API_KEY}` }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
-          >
-            <AnyReactComponent
-              lat={lat}
-              lng={lng}
-              text={this.props.listing.name}
-            />
-          </GoogleMapReact>
-        </div>
-      );
+        <center>
+          <div style={{ height: '50vh', width: '50%' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: `${process.env.REACT_APP_API_KEY}` }}
+              defaultCenter={center}
+              defaultZoom={this.props.zoom}
+            >
+            <Marker lat={lat} lng={lng} />
+            </GoogleMapReact>
+          </div>
+        </center>
+    );
     }
 
 }
+
+
 
 function mapStateToProps(state) {
   return {
